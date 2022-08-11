b30c5e19a522c555ae77e22dda594dfe92606ed9
SkochkoT <tapasy81@gmail.com>
2022-08-11 21:44:02 +0300

    Deleted choose forecast

--- src/index.js
+++ src/index.js
@@ -28,8 +28,6 @@ function showForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  //let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
@@ -62,10 +60,8 @@ function showForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c475d7b28899306359b30750ca522e5c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(showForecast);
 }
 
@@ -151,4 +147,3 @@ let currentPosition = document.querySelector("#current-location");
 currentPosition.addEventListener("click", currentLocation);
 
 search("Kyiv");
-//showForecast();
